d40cecda800daa29c3b37d63c5867acc1c125680
mastershubham <74030142+mastershubham@users.noreply.github.com>
2022-10-07 22:28:20 +0530

    Static Range Minimum Queries | CSES | RMQ | Sparse Table

---
+++ StaticRangeMinimumQueries.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+const int MAX_N = 200'001;
+const int LOG = 18;
+int a[MAX_N];
+int mem[MAX_N][LOG];
+int bin_log[MAX_N];
+ 
+int query(int l,int r){
+    int range = r-l+1;
+    int j = bin_log[range];
+    return min(mem[l][j],mem[r+1-(1<<j)][j]);
+}
+int main(){
+    int n,q;
+    cin>>n>>q;
+    bin_log[1] = 0;
+    for(int i = 2;i<=n;i++){
+        bin_log[i] = bin_log[i/2] + 1;
+    }
+ 
+    for(int i = 0;i<n;i++){
+        cin>>a[i];
+        mem[i][0] = a[i];
+    }
+ 
+    for(int j = 1;j<LOG;j++){
+        for(int i = 0;i-1+(1<<j)<n;i++){
+            mem[i][j] = min(mem[i][j-1],mem[i+(1<<(j-1))][j-1]);
+        }
+    }
+    while(q--){;
+        int l,r;
+        cin>>l>>r;
+        l--;r--;
+        cout<<query(l,r)<<'\n';
+    }
+}
